72c02a65e8f31e57b807e2cf9fda3fad86d9d9a6
KLokeshTech <143431098+KLokeshTech@users.noreply.github.com>
2026-04-14 13:33:48 +0530

    Add files via upload

---
+++ python_basics_kv_lokesh.py
@@ -0,0 +1,17 @@
+#Task 1: Variables and Data Types
+Name = "lokesh"
+Age = 32
+Height=5.8
+is_student = True
+
+print(Name,Age,Height,is_student)
+
+#Task 2: String Formatting
+print(f'Name:{Name} | Age: {Age} | Height: {Height} | Student: {is_student}')
+
+#Task 3: Arithmetic Operations
+age_months=Age*12
+Age_days=Age*365
+age_div_7=Age%7
+age_pow_2=Age**2
+print(f'Age in months:{age_months} | Age in days:{Age_days} | remainder when age is divided by 7:{age_div_7} | Age raised to the power of 2:{age_pow_2}')
